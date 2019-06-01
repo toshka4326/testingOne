4351bf2d2fcf1e1dd1519becd5bfe5ff65e6088b
toshka4326 <51108949+toshka4326@users.noreply.github.com>
2019-06-01 14:44:43 +0700

    refatory

--- src/test/java/ru/raspYandex/OneTest.java
+++ src/test/java/ru/raspYandex/OneTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class OneTest extends WebDriverSettings {
@@ -13,21 +15,21 @@ public class OneTest extends WebDriverSettings {
 
         driver.get("https://rasp.yandex.ru/");
 
-        WebElement form = driver.findElement(By.xpath("//*[@id=\"root\"]/div/header/div[1]/div/div[5]/form"));
+        WebElement form = driver.findElement(By.cssSelector("[class=\"SearchForm\"]"));
 
         WebDriverWait wait = new WebDriverWait(driver, 10);
 
         form.findElement(By.id("from")).sendKeys("Кемерово");
-
         form.findElement(By.id("to")).sendKeys("Москва");
-
         form.findElement(By.id("when")).sendKeys("7 июля");
-
         form.findElement(By.xpath("//*[@id=\"root\"]/div/header/div[1]/div/div[5]/form/button[2]")).click();
+
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("section")));
+
     }
 
-    /*@Test
-    public void twoTest () {
+   /* @Test
+    public void testTripName () {
 
 
 
