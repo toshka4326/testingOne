7d68cfd4ca6f046dd4fa78da44451b5c48bf35e5
toshka4326 <51108949+toshka4326@users.noreply.github.com>
2019-06-01 18:43:54 +0700

    added comments and 9

--- src/test/java/ru/raspYandex/OneTest.java
+++ src/test/java/ru/raspYandex/OneTest.java
@@ -1,42 +1,43 @@
 package ru.raspYandex;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class OneTest extends WebDriverSettings {
 
     @Test
     public void oneTest () {
 
+        //1. Пользователь открывает сайт https://rasp.yandex.ru
         driver.get("https://rasp.yandex.ru/");
-
         WebElement form = driver.findElement(By.cssSelector("[class=\"SearchForm\"]"));
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("[class=\"SearchForm\"]")));
 
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-
+        //2. Вводит пункт отправления «Кемерово»
         form.findElement(By.id("from")).sendKeys("Кемерово");
-        form.findElement(By.id("to")).sendKeys("Москва");
-        form.findElement(By.id("when")).sendKeys("7 июля");
-        form.findElement(By.xpath("//*[@id=\"root\"]/div/header/div[1]/div/div[5]/form/button[2]")).click();
 
+        //3. Вводит пункт назначения «Москва»
+        form.findElement(By.id("to")).sendKeys("Москва");
+
+        //4. Вводит дату 7 июля
+        form.findElement(By.id("when")).sendKeys("7 июля");
+
+        //5. Нажимает Найти
+        form.findElement(By.xpath("//*[@id=\"root\"]/div/header/div[1]/div/div[5]/form/button[2]")).click();
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("section")));
 
+        //6. Проверяет, что отображается название рейса.
+
+        //7. Проверяет, что у направления есть время в пути.
+        //8. Проверяет, что у всех рейсов есть иконка транспорта.
+
+        //9. Проверяет, что рейсов отобразилось 5.
+        int count = driver.findElements(By.cssSelector("h3")).size();
+        Assert.assertEquals(5,count);
     }
-
-   /* @Test
-    public void testTripName () {
-
-
-
-
-
-
-    }*/
-
-
 }
+
+
--- src/test/java/ru/raspYandex/WebDriverSettings.java
+++ src/test/java/ru/raspYandex/WebDriverSettings.java
@@ -2,22 +2,26 @@ package ru.raspYandex;
 
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebDriverSettings {
 
-    public ChromeDriver driver;
+    public WebDriver driver;
+    public WebDriverWait wait;
 
     @Before
     public void preTest () {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver74.0.3729.6.exe");
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
         System.out.println("Тест запущен");
     }
 
-    @After
+    /*@After
     public void quit() {
         System.out.println("Тест окончен");
-        //driver.quit();
-    }
+        driver.quit();
+    }*/
 }
